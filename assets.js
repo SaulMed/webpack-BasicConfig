5df84acede1956e8eb1d371a494fd9331b8b081f
Saul Medina <elesedelsaul@gmail.com>
2020-06-06 15:02:24 -0500

    First Basic Config With Webpack

---
+++ assets.js
@@ -0,0 +1,11 @@
+const JS = [
+    'bootstrap/dist/js/bootstrap.min.js',
+    'jquery/dist/jquery.min.js'
+]
+
+const CSS = [
+    'animate.css/animate.min.css',
+    'bootstrap/dist/css/bootstrap.min.css'
+]
+
+module.exports = [...JS, ...CSS]    //Exportar un arreglo con 2 arreglos
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+//Archivos importados
+const assets = require('./assets')
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: assets.map(asset => {
+                return {
+                    from: path.resolve(__dirname, `./node_modules/${asset}`),
+                    to: path.resolve(__dirname, 'libs')
+                }
+            })
+        })
+    ]
+}
